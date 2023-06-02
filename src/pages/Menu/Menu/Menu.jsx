989ff3c95168a325f06c227601460ede2ea4ed11
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-06-02 21:03:20 +0600

    bistro boss

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,21 @@
 import { Helmet  } from 'react-helmet-async';
-
+import MenuCover from '../../Shared/MenuCover/MenuCover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopulerMenu from '../../Home/PopulerMenu/PopulerMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                <title> Bistro Boss | Menu</title>
             </Helmet>
-            <h1>mabny many menu </h1>
+            <MenuCover img={menuImg} tittle='our menu' />
+            <PopulerMenu/>
+            <MenuCover img={menuImg} tittle='our menu' />
+            <PopulerMenu/>
+            <MenuCover img={menuImg} tittle='our menu' />
+            <PopulerMenu/>
+            <MenuCover img={menuImg} tittle='our menu' />
+            <PopulerMenu/>
         </div>
     );
 };
